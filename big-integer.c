8630aa33e801838f1638bf051f2b79d6a33e0df2
HaoyangQi <haoyangq@gmail.com>
2024-02-08 21:07:31 -0500

    IEEE 754-2008 and big integer library

---
+++ big-integer.c
@@ -0,0 +1,776 @@
+#include "big-integer.h"
+
+#define __bi_c2d(c) ((int)((char)(c) - '0'))
+#define __bi_d2c(d) ((char)((d) + '0'))
+
+static __bi_grow(big_integer* bn, size_t by, bi_grow grow)
+{
+    size_t len = strlen(bn->raw);
+    size_t sz = sizeof(char) * (len + by + 1);
+    char* n = (char*)malloc_assert(sz);
+
+    memset(n, 0, sz);
+
+    // move it
+    if (grow == BI_RAW_GROW_LEFT)
+    {
+        memcpy(n + by, bn->raw, sizeof(char) * len);
+    }
+    else
+    {
+        memcpy(n, bn->raw, sizeof(char) * len);
+    }
+
+    // fill '0' to make sense out of it
+    for (size_t f = 0; f < by; f++)
+    {
+        n[len + f] = '0';
+    }
+
+    // terminate
+    n[len + by] = 0;
+
+    free(bn->raw);
+    bn->raw = n;
+}
+
+/**
+ * return most significant digit index
+ *
+ * if number is 0, index returned is length of number
+*/
+size_t __bi_idx_msb(const big_integer* b)
+{
+    size_t len = strlen(b->raw);
+    size_t i;
+
+    // skip leading 0's
+    for (i = 0; i < len; i++)
+    {
+        if (b->raw[i] != '0')
+        {
+            break;
+        }
+    }
+
+    return i;
+}
+
+static big_integer* __bi_substr(const big_integer* b, size_t from, size_t len)
+{
+    size_t blen = strlen(b->raw);
+
+    if (from >= blen || len == 0) return NULL;
+
+    big_integer* n = (big_integer*)malloc_assert(sizeof(big_integer));
+    len = min(len, blen); // make sure length does not overflow
+    n->raw = (char*)malloc_assert(sizeof(char) * (len + 1));
+    n->raw[len] = 0;
+
+    memcpy(n->raw, b->raw + from, sizeof(char) * len);
+    return n;
+}
+
+static __bi_dup(big_integer* dest, const big_integer* src)
+{
+    size_t sz = sizeof(char) * (strlen(src->raw) + 1);
+    dest->raw = (char*)malloc_assert(sz);
+    dest->raw[strlen(src->raw)] = 0;
+    memcpy(dest->raw, src->raw, sz);
+}
+
+/**
+ * duplicate length of src and fill all digits with same one
+*/
+static __bi_dup_shape(big_integer* dest, const big_integer* src, const unsigned int fill)
+{
+    if (fill > 9) return;
+
+    size_t len = strlen(src->raw);
+    size_t i = 0;
+
+    dest->raw = (char*)malloc_assert(sizeof(char) * (len + 1));
+
+    for (i = 0; i < len; i++)
+    {
+        dest->raw[i] = __bi_d2c(fill);
+    }
+
+    dest->raw[i] = 0;
+}
+
+void __bi_assign(big_integer* dest, const big_integer* src)
+{
+    char* rd = dest->raw;
+    char* rs = src->raw;
+    size_t len1 = strlen(rd);
+    size_t len2 = strlen(rs);
+
+    if (len1 < len2)
+    {
+        release_big_integer(dest);
+        __bi_dup(dest, src);
+        return;
+    }
+
+    for (size_t i = len1 - 1, j = len2 - 1; i >= 0; i--)
+    {
+        rd[i] = (len1 - i == len2 - j) ? rs[j] : '0';
+
+        if (j > 0) j--;
+    }
+}
+
+void __bi_swap(big_integer* dest, big_integer* src)
+{
+    char* tmp = dest->raw;
+
+    dest->raw = src->raw;
+    src->raw = tmp;
+}
+
+/**
+ * initialize a 1-digit integer
+*/
+void init_big_integer(big_integer* bn, unsigned int digit)
+{
+    bn->raw = (char*)malloc_assert(sizeof(char) * 2);
+    bn->raw[0] = __bi_d2c(digit % 10);
+    bn->raw[1] = '\0';
+}
+
+void release_big_integer(big_integer* bn)
+{
+    free(bn->raw);
+}
+
+/**
+ * single-digit addition
+ *
+ * maximum carry: 1
+*/
+void bi_dadd(big_integer* dest, const unsigned int digit)
+{
+    if (digit > 9) return;
+
+    char* r = dest->raw;
+    int carry = digit;
+    int full;
+
+    for (size_t i = strlen(r) - 1; i >= 0; i--)
+    {
+        full = __bi_c2d(r[i]) + carry;
+        carry = full / 10;
+        r[i] = __bi_d2c(full % 10);
+    }
+
+    // now MSB determines if we need to grow
+    if (carry)
+    {
+        bi_append_digit(dest, __bi_d2c(carry), BI_RAW_GROW_LEFT);
+    }
+}
+
+/**
+ * single-digit subtraction
+ *
+ * maximum carry: 1
+ *
+ * funtion returns true if result is negative
+*/
+bool bi_dsub(big_integer* dest, const unsigned int digit)
+{
+    if (digit > 9) return;
+
+    char* r = dest->raw;
+    int carry = digit;
+    int full;
+    size_t len = strlen(r);
+
+    // simple case: dest is also single digit
+    if (len - __bi_idx_msb(dest) <= 1)
+    {
+        int result = __bi_c2d(r[0]) - (int)digit;
+        r[0] = __bi_d2c(abs(result));
+
+        return result < 0;
+    }
+
+    for (size_t i = len - 1; i >= 0; i--)
+    {
+        full = __bi_c2d(r[i]) - carry;
+        carry = full < 0 ? 1 : 0;
+        r[i] = __bi_d2c(full < 0 ? (full + 10) : full);
+    }
+
+    return false;
+}
+
+/**
+ * single-digit multiplication
+ *
+ * maximum carry: 8
+*/
+void bi_dmul(big_integer* dest, const unsigned int digit)
+{
+    if (digit > 9)
+    {
+        return;
+    }
+
+    char* r = dest->raw;
+    int carry = 0;
+    int full;
+
+    for (size_t i = strlen(r) - 1; i >= 0; i--)
+    {
+        full = __bi_c2d(r[i]) * digit + carry;
+        carry = full / 10;
+        r[i] = __bi_d2c(full % 10);
+    }
+
+    // now MSB determines if we need to grow
+    if (carry)
+    {
+        bi_append_digit(dest, __bi_d2c(carry), BI_RAW_GROW_LEFT);
+    }
+}
+
+/**
+ * digit division where denominator can only be value 1 - 9
+ * find a maximum digit such that: denom * digit <= num
+ *
+ * remainder can only be one digit
+*/
+void bi_ddiv(const big_integer* num, const unsigned int denom, big_integer* q, big_integer* r)
+{
+    init_big_integer(q, 0);
+    init_big_integer(r, 0);
+
+    if (denom == 0 || denom > 9) return;
+
+    if (denom == 1)
+    {
+        // q = num, r = 0
+        release_big_integer(q);
+        __bi_dup(q, num);
+        return;
+    }
+
+    char* rd = num->raw;
+    size_t len = strlen(rd);
+    int __r = 0;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        int __v = __r * 10 + __bi_c2d(rd[0]);
+        int __r = __v % denom;
+
+        bi_append_digit(q, __bi_d2c(__v / denom), BI_RAW_GROW_RIGHT);
+    }
+
+    r->raw[0] = __bi_d2c(__r);
+}
+
+/**
+ * multiply by 2
+ *
+ * digit adder yield result has at most 2 digits
+ * carry <= 1
+*/
+void bi_mul2(big_integer* dest)
+{
+    bi_dmul(dest, 2);
+}
+
+/**
+ * multiply by 10
+ *
+ * simply grow on the right and fill trailing '0'
+*/
+void bi_mul10(big_integer* dest)
+{
+    size_t len = strlen(dest->raw);
+    size_t sz = sizeof(char) * (len + 1);
+    size_t i = __bi_idx_msb(dest);
+
+    if (i == len)
+    {
+        // if 0
+        return;
+    }
+    else if (i != 0)
+    {
+        // if we have leading 0s, move content
+        char* tmp = (char*)malloc_assert(sz);
+        memcpy(tmp, dest->raw, sz);
+        memcpy(dest->raw, tmp + 1, sz - sizeof(char));
+        dest->raw[len - 1] = '0';
+    }
+    else
+    {
+        // otherwise we need to grow
+        __bi_grow(dest->raw, 1, BI_RAW_GROW_RIGHT);
+    }
+}
+
+/**
+ * divide by 2
+ *
+ * produce d = 2 * q + r
+ *
+ * remainder is always one digit
+*/
+void bi_div2(const big_integer* d, big_integer* q, big_integer* r)
+{
+    bi_ddiv(d, 2, q, r);
+}
+
+/**
+ * divide by 10
+ *
+ * produce d = 10 * q + r
+ * q is d except last digit
+ * r is last digit of d
+*/
+void bi_div10(const big_integer* d, big_integer* q, big_integer* r)
+{
+    init_big_integer(q, 0);
+    init_big_integer(r, 0);
+
+    r->raw[0] = d->raw[strlen(d->raw) - 1];
+
+    size_t sz = sizeof(char) * (strlen(d->raw));
+    q->raw = (char*)malloc_assert(sz);
+    memset(q->raw, 0, sz);
+    memcpy(q->raw, d->raw, sz);
+}
+
+/**
+ * multiply by power of 10
+ *
+ * simply grow on the right and fill trailing '0'
+*/
+void bi_mulpow10(big_integer* dest, size_t e)
+{
+    size_t len = strlen(dest->raw);
+    size_t i = __bi_idx_msb(dest);
+
+    if (i == len || e == 0)
+    {
+        // if 0 or no grow
+        return;
+    }
+
+    // if leading zero count is less than required,
+    // grow by delta
+    if (i < e)
+    {
+        __bi_grow(dest->raw, e - i, BI_RAW_GROW_LEFT);
+
+        // align data
+        len = strlen(dest->raw);
+        i += e;
+    }
+
+    // now move content to left by e times
+    size_t sz = sizeof(char) * (len + 1);
+    char* tmp = (char*)malloc_assert(sz);
+    memcpy(tmp, dest->raw, sz);
+    memcpy(dest->raw + i - e, tmp + i, sizeof(char) * (len - i));
+
+    // for trailing, set to 0
+    for (i = len - e; i < len; i++)
+    {
+        dest->raw[i] = '0';
+    }
+
+    dest->raw[len] = 0;
+}
+
+/**
+ * divide by power of 10
+ *
+ * produce d = 10^e * q + r
+ * q is first len(d) - e digits
+ * r is d except q part
+ *
+ * maximum of 10^e is maximum cha we can access in raw, so size_t is sufficient
+*/
+void bi_divpow10(const big_integer* d, size_t e, big_integer* q, big_integer* r)
+{
+    size_t len = strlen(d->raw);
+    size_t sz = sizeof(char) * (len + 1);
+
+    // if e is 0, quotient is d, remainder is 0
+    if (e == 0)
+    {
+        q->raw = (char*)malloc_assert(sz);
+        q->raw[len] = 0;
+        memcpy(q->raw, d->raw, sz);
+
+        init_big_integer(q, 0);
+        return;
+    }
+
+    // if e is too big, quotient is 0, remainder is d
+    if (e >= len)
+    {
+        r->raw = (char*)malloc_assert(sz);
+        r->raw[len] = 0;
+        memcpy(r->raw, d->raw, sz);
+
+        init_big_integer(q, 0);
+        return;
+    }
+
+    sz = sizeof(char) * (len - e + 1);
+    q->raw = (char*)malloc_assert(sz);
+    q->raw[len - e] = '\0';
+    memcpy(q->raw, d->raw, sz);
+
+    if (e == 1)
+    {
+        init_big_integer(r, __bi_c2d(d->raw[len - 1]));
+    }
+    else
+    {
+        sz = sizeof(char) * (e + 1);
+        r->raw = (char*)malloc_assert(sz);
+        r->raw[e] = '\0';
+        memcpy(r->raw, d->raw + len - e, sz);
+    }
+}
+
+/**
+ * append right: dest * 10 + digit
+ * append left: MSB = digit
+*/
+void bi_append_digit(big_integer* dest, const char digit, bi_grow grow)
+{
+    if (!isdigit(digit)) return;
+
+    if (grow == BI_RAW_GROW_RIGHT)
+    {
+        bi_mul10(dest);
+        dest->raw[strlen(dest->raw) - 1] = digit;
+    }
+    else
+    {
+        if (digit == '0') return;
+
+        size_t i = __bi_idx_msb(dest);
+
+        // if there is no space, grow
+        if (i == 0)
+        {
+            __bi_grow(dest, 1, BI_RAW_GROW_LEFT);
+            i = 1;
+        }
+
+        // push digit
+        dest->raw[i - 1] = digit;
+    }
+}
+
+bi_compare bi_cmp(const big_integer* b1, const big_integer* b2)
+{
+    size_t len1 = strlen(b1->raw);
+    size_t len2 = strlen(b2->raw);
+    size_t i = __bi_idx_msb(b1);
+    size_t j = __bi_idx_msb(b2);
+
+    // longer number is bigger
+    if (len1 - i > len2 - j)
+    {
+        // b1 > b2
+        return BI_CMP_GREATER;
+    }
+    else if (len1 - i < len2 - j)
+    {
+        // b1 < b2
+        return BI_CMP_LESS;
+    }
+
+    // if length match, compare digit-wise
+    for (;i < len1; i++, j++)
+    {
+        // compare ascii is enough
+        if (b1->raw[i] > b2->raw[j])
+        {
+            // b1 > b2
+            return BI_CMP_GREATER;
+        }
+        else if (b1->raw[i] < b2->raw[j])
+        {
+            // b1 < b2
+            return BI_CMP_LESS;
+        }
+    }
+
+    // here, numbers are equal
+    return BI_CMP_EQUAL;
+}
+
+/**
+ * addition
+ *
+ * b1 = b1 + b2
+*/
+void bi_add(big_integer* b1, const big_integer* b2)
+{
+    char* r1 = b1->raw;
+    char* r2 = b2->raw;
+    size_t len1 = strlen(r1);
+    size_t len2 = strlen(r2);
+    int carry = 0;
+
+    // resize b1 if len1 < len2 because it has to be done anyway
+    if (len1 < len2)
+    {
+        __bi_grow(b1, len2 - len1, BI_RAW_GROW_LEFT);
+
+        // need to redo this due to potential pointer change
+        r1 = b1->raw;
+        len1 = strlen(r1);
+    }
+
+    // now len1 >= len2
+    for (size_t i = len1 - 1, j = len2 - 1; i >= 0; i--)
+    {
+        int v = __bi_c2d(r1[i]) + (j >= 0 ? __bi_c2d(r2[j]) : 0) + carry;
+        carry = v / 10;
+        r1[i] = __bi_d2c(v % 10);
+
+        if (j > 0) j--;
+    }
+
+    // fill carry
+    if (carry)
+    {
+        bi_append_digit(b1, '1', BI_RAW_GROW_LEFT);
+    }
+}
+
+/**
+ * subtraction
+ *
+ * b1 = |b1 - b2|, returns true if the value is negative
+*/
+bool bi_sub(big_integer* b1, const big_integer* b2)
+{
+    big_integer s1, s2;
+    bi_compare cmp = bi_cmp(b1, b2);
+    int carry = 0;
+
+    // pointer is easier for swaping
+    big_integer* ps1 = &s1;
+    big_integer* ps2 = &s2;
+
+    // initialize copy if parameters
+    __bi_dup(ps1, b1);
+    __bi_dup(ps2, b2);
+
+    // we always subtract larger one with smaller one
+    if (cmp == BI_CMP_LESS)
+    {
+        // swap
+        big_integer* tmp = ps1;
+        ps1 = ps2;
+        ps2 = tmp;
+    }
+
+    // get iterate data
+    char* r1 = ps1->raw;
+    char* r2 = ps2->raw;
+    size_t len1 = strlen(ps1->raw);
+    size_t len2 = strlen(ps2->raw);
+
+    // subtract, len1 >= len2
+    for (size_t i = len1 - 1, j = len2 - 1; i >= 0; i--)
+    {
+        int v = __bi_c2d(r1[i]) - (j >= 0 ? __bi_c2d(r2[j]) : 0) - carry;
+        carry = v < 0 ? 1 : 0;
+        r1[i] = __bi_d2c(v < 0 ? (10 + v) : v);
+
+        if (j > 0) j--;
+    }
+
+    // move result to b1
+    release_big_integer(b1);
+    b1->raw = ps1->raw;
+    s1.raw = NULL;
+
+    // delete temp variables
+    release_big_integer(ps1);
+    release_big_integer(ps2);
+
+    return cmp == BI_CMP_LESS;
+}
+
+/**
+ * division
+ *
+ * q, r will have max length to be length of numerator
+ * q, r may have leading 0s
+ *
+ * if div by 0, q=0 and r=0
+*/
+void bi_div(const big_integer* num, const big_integer* denom, big_integer* q, big_integer* r)
+{
+    char* rn = num->raw;
+    char* rd = denom->raw;
+    size_t len1 = strlen(rn);
+    size_t len2 = strlen(rd);
+    size_t msb_denom = __bi_idx_msb(denom);
+
+    // edge and optimized cases
+    if (msb_denom == len2)
+    {
+        // div by 0
+        init_big_integer(q, 0);
+        init_big_integer(r, 0);
+        return;
+    }
+    else if (msb_denom == len2 - 1)
+    {
+        // div by a digit
+        bi_ddiv(num, __bi_c2d(rd[len2 - 1]), q, r);
+        return;
+    }
+
+    // initialize
+    __bi_dup_shape(q, num, 0);
+    __bi_dup_shape(r, num, 0);
+
+    for (size_t i = 0; i < len1; i++)
+    {
+        // calculate new candidate numerator: r * 10 + digit
+        // CANNOT use append here because remainder may change
+        bi_mul10(r);
+        bi_dadd(r, __bi_c2d(rn[i]));
+
+        /**
+         * now quotient digit can be calculated in the following:
+         *
+         * since: r / denom = digit, remainder r'
+         * so we can simplify the division into:
+         *
+         * r / digit + r' = denom
+         *
+         * so it means if digit is a maximum digit that makes
+         * following true, then the digit is the quotient:
+         *
+         * r / digit >= denom
+         *
+         * then the new remainder is: r - denom * digit
+        */
+
+        unsigned int digit;
+        big_integer __q, __r;
+        bi_compare __cmp = BI_CMP_LESS;
+
+        // find quotient digit
+        for (digit = 9, __cmp = BI_CMP_LESS; digit > 0 && __cmp == BI_CMP_LESS; digit--)
+        {
+            bi_ddiv(r, digit, &__q, &__r);
+            __cmp = bi_cmp(&__q, denom);
+            release_big_integer(&__q);
+            release_big_integer(&__r);
+        }
+
+        // if quotient digit non-zero, remainder needs to be updated
+        if (digit != 0)
+        {
+            __bi_dup(&__r, denom);
+            bi_dmul(&__r, digit);
+            bi_sub(r, &__r);
+            release_big_integer(&__r);
+        }
+
+        // append quotient digit
+        bi_append_digit(q, __bi_d2c(digit), BI_RAW_GROW_RIGHT);
+    }
+}
+
+/**
+ * count bits of binary form
+*/
+int64_t bi_count_bits(const big_integer* d, bool* success)
+{
+    big_integer _d, q, r;
+    int64_t c = 0;
+
+    // initialize
+    __bi_dup(&_d, d);
+
+    while (true)
+    {
+        bi_div2(&_d, &q, &r);
+
+        // prepare for next iteration
+        __bi_swap(&_d, &q);
+        release_big_integer(&q);
+        release_big_integer(&r);
+
+        // test if quotient is 0, if so we stop
+        if (strlen(q.raw) == __bi_idx_msb(&q))
+        {
+            release_big_integer(&_d);
+            break;
+        }
+        else if (c == INT64_MAX)
+        {
+            if (success) *success = false;
+            break;
+        }
+
+        c++;
+    }
+
+    if (success) *success = true;
+
+    return c;
+}
+
+/**
+ * get bits from LSB
+ *
+ * luckily, long division can be applied here because
+ * it generates bits from LSB
+*/
+uint64_t bi_truncate(const big_integer* d, size_t bits)
+{
+    uint64_t v = 0;
+    bits = min(bits, 64);
+
+    big_integer _d, q, r;
+
+    // initialize
+    __bi_dup(&_d, d);
+
+    for (size_t i = 0; i < bits; i++)
+    {
+        bi_div2(&_d, &q, &r);
+
+        // prepare for next iteration
+        __bi_swap(&_d, &q);
+        release_big_integer(&q);
+        release_big_integer(&r);
+
+        // test if quotient is 0, if so we stop
+        if (strlen(q.raw) == __bi_idx_msb(&q))
+        {
+            release_big_integer(&_d);
+            break;
+        }
+
+        // otherwise, we write the bit
+        if (r.raw[0] == '1')
+        {
+            v |= (1 << i);
+        }
+    }
+
+    return v;
+}
--- ir-literal.c
+++ ir-literal.c
@@ -1,11 +1,35 @@
 #include "ir.h"
+#include "big-integer.h"
 
 #define SELECT_INT_DATA_SIZE(bits) ((bits) == 32 ? IRPV_INTEGER_BIT_32 : IRPV_INTEGER_BIT_64)
+#define U64_CLEAR_MSB_MASK ((uint64_t)0x7FFFFFFFFFFFFFFF) // ~((uint64_t)1 << 63)
 
 // primitive data bit-length
 static const int primitive_bit_length[IRPV_MAX] = { 8, 16, 32, 64, 16, 32, 64, 32 };
+
 // number type to base
 static const int number_base[JT_NUM_MAX] = { 10, 16, 8, 2, 10, 10 };
+
+// big integer constants
+static const big_integer two_pow_23 = { .raw = "8388608" };
+static const big_integer two_pow_52 = { .raw = "4503599627370496" };
+
+static bool __int64_sum_safe(int64_t a, int64_t b)
+{
+    /**
+     * oh, swap without additional variable...
+     *
+     * uggggggh, no thank you (guess why)
+    */
+    if (a < b)
+    {
+        int64_t tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    return a < 0 || INT64_MAX - a < b;
+}
 
 /**
  * safe arithmetic for 64-bit integer: n * base + digit
@@ -184,129 +208,279 @@ static uint64_t __u64_parse_generic(java_ir* ir, const char* content, java_numbe
 }
 
 /**
- * IEEE 754 Standard
+ * IEEE 754-2008 Standard
  *
  * Single Precision:
  * sign: 1 bit
  * exponent: 8 bits
- * mantissa: 23 bits
+ * fraction: 23 bits
  *
  * Double Precision:
  * sign: 1 bit
  * exponent: 11 bits
- * mantissa: 52 bits
+ * fraction: 52 bits
+ *
+ * Logic (Big-Number Arithmetic):
+ * 1. use big number to store original content in form: p = I * 10^e
+ *    where I and 10^e are both big number
+ *    denominator = e < 0 : 10^e : 1
+ * 2. scaling p to IEEE fraction bit length: I * 2^s * 10^e
+ *    scale factor: s
+ *    float  => [2^23, 2^24)
+ *    double => [2^52, 2^53)
+ * 3. still big number, find quotient and remainder of scaled p: q * 10^e + r
+ * 4. round q up/down to next integer (hence +/-1)
+ *    r < 0.5 * denominator: up
+ *    r > 0.5 * denominator: down
+ *    r = 0.5 * denominator: down if q is even, up if q is odd
+ *    now the q is significand
+ * 5. edge case: if q = upper bound (2^24 or 2^53), let q = lower bound,
+ *    and s--
+ * 6. convert significand to binary: bin(q)
+ * 7. cancel scale factor: bin(q) * 2^(-s)
+ * 8. use binary scientific notation to simplify: bin(q) contains power of fraction bit length
+ *    so: bin(q') * 2^(52|23) * 2(-s) = bin(q') * 2^(52|23 - s)
+ *    q' is not useful so no need to maintain that part
+ * 9. put together
+ *    sign: always 0 (positive)
+ *    exponent (signed): 2^(11|8) - 1 + 2^(52|23 - s)
+ *    fraction: q - 2^(52|23), and the trailing 52 or 23 bits
 */
 static uint64_t __u64_parse_ieee754(java_ir* ir, const char* content, bool single_precision)
 {
     size_t len = strlen(content);
     size_t bl_exp = single_precision ? 8 : 11;
-    size_t bl_man = single_precision ? 23 : 52;
+    size_t bl_frac = single_precision ? 23 : 52;
+    int64_t exp_bias = single_precision ? 127 : 1023; // 2^(bl_exp - 1) - 1
     uint64_t n = 0;
+
+    big_integer I, q, r;
+    int64_t exp10 = 0, exp10_part = 0, s;
+    size_t i;
     bool success;
+    char c;
 
-    /**
-     * 1. find decimal place in string
-     *
-     * because of the exponent part, the decimal place may move
-     *
-     * if it is negative, it means more leading 0's needs to be filled
-     * in decimal part
-     *
-     * assumption: there is only at most one '.', at most one 'e'
-    */
-    int64_t idx_decimal = len;
-    int64_t decimal_shift = 0;
-    success = true;
-    for (size_t i = 0; success && i < len; i++)
+    init_big_integer(&I, 0);
+
+    // first get I and fraction scale info
+    for (i = 0; i < len; i++, exp10--)
     {
-        switch (content[i])
+        c = content[i];
+
+        // exponent part needs to be parsed differently
+        if (c == 'e' || c == 'E')
         {
-            case '.':
-                // reach decimal
-                idx_decimal = i;
-                i++;
-
-                // start skipping until end or exponent
-                for (; i < len; i++)
-                {
-                    if (tolower(content[i]) == 'e')
-                    {
-                        break;
-                    }
-                }
-
-                // make sure next is 'e'
-                i--;
-                continue;
-            case 'e':
-            case 'E':
-                // reach exponent part, read it
-                decimal_shift = s2n(content + idx_decimal + 1, JT_NUM_DEC, &success);
-
-                if (!success)
-                {
-                    ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
-                }
-
-                if (content[idx_decimal + 1] == '-')
-                {
-                    decimal_shift = -decimal_shift;
-                }
-
-                break;
-            default:
-                break;
+            break;
         }
+
+        if (c == '.')
+        {
+            exp10 = 0;
+            continue;
+        }
+
+        bi_append_digit(&I, c, BI_RAW_GROW_RIGHT);
+    }
+
+    // adjust fraction scale factor
+    if (exp10 == i && (i >= len || tolower(c) == 'e'))
+    {
+        exp10 = 0;
     }
 
     /**
-     * 2. calculate actual decimal index
+     * if we have exponent part, parse it
      *
-     * idx_decimal <= 0 means:
-     * 1. no whole number part, AND
-     * 2. there are abs(idx_decimal) number of additioanl leading 0s
-     *    in decimal part
-     *
-     * idx_decimal >= len means:
-     * 1. no decimal part
-     * 2. whole part needs to times 10^(idx_decimal - len)
+     * since 10^e has (e+1) digits, so e is the max length
+     * of big integer 10^e, so size_t is sufficient
     */
-    idx_decimal -= decimal_shift;
-
-    /**
-     * 2. convert whole number part
-     *
-     * for the positive part of idx_decimal - len:
-     * the whole part expands whole * 10^(idx_decimal - len)
-    */
-    uint64_t part_whole = 0;
-    if (idx_decimal > 0)
+    if (i < len && tolower(c) == 'e')
     {
-        part_whole = s2n(content, JT_NUM_DEC, &success);
+        bool exp10_part_neg = false;
 
-        for (int64_t i = idx_decimal - len; success && i > 0; i++)
+        i++;
+        c = content[i];
+
+        if (!isdigit(c))
         {
-            part_whole = __int64_digits_safe_append(part_whole, JT_NUM_DEC, 0, &success);
+            exp10_part_neg = c == '-';
+            i++;
         }
+
+        exp10_part = s2n(content + i, JT_NUM_DEC, &success);
 
         if (!success)
         {
-            ir_error(ir, JAVA_E_PART_WHOLE_OVERFLOW);
+            ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+            goto end;
+        }
+
+        if (exp10_part_neg)
+        {
+            exp10_part = -exp10_part;
         }
     }
 
     /**
-     * 3. convert decimal part
-     * TODO:
+     * combine 2 parts of exponent gives us final 10^e
+     * TODO: is overflow check correct?
     */
-    uint64_t part_decimal = 0;
-    if (idx_decimal < len)
+    if (__int64_sum_safe(exp10, exp10_part))
     {
+        ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+        goto end;
+    }
+    exp10 += exp10_part;
+
+    /**
+     * further combination: if e > 0, merge into I
+    */
+    if (exp10 > 0)
+    {
+        bi_mulpow10(&I, exp10);
+        exp10 = 0;
     }
 
     /**
-     * TODO: n bound check
+     * find s = log2(I)
+     * where s if the largest value such that: 2^s <= I
+     * therefore: s is number of bits of I in binary
+     *
+     * if e < 0, we take quotient of I * 10^e
+     * to find the logrithm
     */
+    if (exp10 != 0)
+    {
+        big_integer qI, rI;
+
+        bi_divpow10(&I, -exp10, &qI, &rI);
+        s = bi_count_bits(&qI, &success);
+
+        release_big_integer(&qI);
+        release_big_integer(&rI);
+    }
+    else
+    {
+        s = bi_count_bits(&I, &success);
+    }
+
+    // give up if log2 is out of bound
+    if (!success)
+    {
+        ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+        goto end;
+    }
+
+    /**
+     * calculate scale factor: s = 23|52 - s
+    */
+    s = (int64_t)bl_frac - s;
+
+    /**
+     * further combination: if s > 0, merge into I
+    */
+    while (s > 0)
+    {
+        bi_mul2(&I);
+        s--;
+    }
+
+    /**
+     * (q, r) = I * 2^s * 10^e
+     *
+     * if any of s and e is negative, a division will occur
+     * otherwise remainder is 0
+     *
+     * the order matter here and 2^s will be calculated first
+    */
+
+    big_integer scale_denom;
+    int64_t __s = s; // we need s later
+    init_big_integer(&scale_denom, 1);
+
+    while (__s < 0)
+    {
+        bi_mul2(&scale_denom);
+        __s++;
+    }
+
+    if (exp10 < 0)
+    {
+        bi_mulpow10(&scale_denom, -exp10);
+    }
+
+    // division will occur one way or another
+    bi_div(&I, &scale_denom, &q, &r);
+    release_big_integer(&scale_denom);
+
+    /**
+     * TODO: round (q +/- 1)
+     *
+     * 2*r < denom: down
+     * 2*r > denom: up
+     * 2*r = denom: down if even, up if odd
+    */
+
+    // count bits of significand: q
+    __s = bi_count_bits(&q, &success);
+
+    /**
+     * TODO:
+     *
+     * s + __s is a huge cancellation, so is
+     * the bound check still necessary here?
+    */
+    if (!success/* || !__int64_sum_safe(__s, s)*/)
+    {
+        ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+        goto end_qr;
+    }
+
+    // find IEEE exponent field
+    s = s + __s + exp_bias;
+
+    /**
+     * exponent range
+     *
+     * exponent part is an unsigned integer in biased form
+     *
+     * valid float range: [0, 255] (8 bits)
+     * valid double range: [0, 2047] (11 bits)
+    */
+    if (single_precision)
+    {
+        if (s < 0 || s > 255)
+        {
+            ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+            goto end_qr;
+        }
+    }
+    else if (s < 0 || s > 2047)
+    {
+        ir_error(ir, JAVA_E_PART_EXPONENT_OVERFLOW);
+        goto end_qr;
+    }
+
+    // calculate full fraction field: q - 2^(23|52)
+    bi_sub(&q, single_precision ? &two_pow_23 : &two_pow_52);
+
+    /**
+     * combine everything together
+     *
+     * sign field: always 0
+     * exponent: 8 or 11 bits (unsigned integer)
+     * fraction field: trailing 23|52 bits of significand
+    */
+    n = ((uint64_t)s << bl_frac) | // exponent
+        bi_truncate(&q, bl_frac) & // fraction
+        U64_CLEAR_MSB_MASK;        // clear sign bit
+
+end_qr:
+    release_big_integer(&q);
+    release_big_integer(&r);
+
+end:
+    release_big_integer(&I);
 
     return n;
 }
